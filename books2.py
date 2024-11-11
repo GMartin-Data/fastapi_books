7a3c4fc11de81cb293f1e6b320b6fc542a5d08b4
Greg <gregory.martin.data@gmail.com>
2024-11-11 21:10:40 +0100

    Implement /books/ GET all books endpoint

---
+++ books2.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+class Book:    
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.title = title
+        self.rating = rating
+
+    
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+]
+
+
+@app.get("/books/")
+def read_all_books():
+    return BOOKS
