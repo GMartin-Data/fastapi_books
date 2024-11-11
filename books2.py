65cdf192cec66a5856f66d63a530e3ab33a73959
Greg <gregory.martin.data@gmail.com>
2024-11-11 21:48:13 +0100

    Add example to swagger UI with model_config

--- books2.py
+++ books2.py
@@ -22,6 +22,17 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(gt=0, lt=6)
+    
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author":"codingwithroby",
+                "description": "A new description of a book",
+                "rating": 5
+            }
+        }
+    }
 
     
 BOOKS = [
