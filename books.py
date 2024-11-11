9ce9cb11d924e3b36ff0b6f50f96f23b3988c87c
Greg <gregory.martin.data@gmail.com>
2024-11-11 19:20:59 +0100

    Reformat with Ruff

--- books.py
+++ books.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI
 
 
 BOOKS = [
-    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
-    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
-    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
-    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+    {"title": "Title One", "author": "Author One", "category": "science"},
+    {"title": "Title Two", "author": "Author Two", "category": "science"},
+    {"title": "Title Three", "author": "Author Three", "category": "history"},
+    {"title": "Title Four", "author": "Author Four", "category": "math"},
+    {"title": "Title Five", "author": "Author Five", "category": "math"},
+    {"title": "Title Six", "author": "Author Two", "category": "math"},
 ]
 
 
@@ -18,11 +18,13 @@ app = FastAPI()
 async def read_all_books():
     return BOOKS
 
+
 @app.get("/books/{book_title}")
 async def read_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():
             return book
+
 
 @app.get("/books/")
 def read_category_by_query(category: str):
@@ -32,12 +34,13 @@ def read_category_by_query(category: str):
             books_from_category.append(book)
     return books_from_category
 
+
 @app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
-        if (book.get("author").casefold() == book_author.casefold()) and \
-        (book.get("category").casefold() == category.casefold()):
+        if (book.get("author").casefold() == book_author.casefold()) and (
+            book.get("category").casefold() == category.casefold()
+        ):
             books_to_return.append(book)
     return books_to_return
-            
